60c54ba833792eb8e26039523324e030c7be6b9f
Damon Burda <damonburda96@gmail.com>
2021-02-26 20:03:53 -0800

    Static files now being served

--- server.js
+++ server.js
@@ -6,12 +6,20 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
+// Sets an empty array to hold notes data
+const notes = [];
+
+// parsing data in JSON format
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Establishing our routes for different pages
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+
+app.use(express.static('public'));
 
 // Sets up server to listen on previously established port 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
