362a5ecea94a45b2fc0d1401017cbd1cc969064c
Damon Burda <damonburda96@gmail.com>
2021-02-26 22:26:44 -0800

    Api storage written

---
+++ db/store.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const util = require('util');
+const uniqid = require('uniqid');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFile('db.json', 'utf8');
+    }
+
+    write(note) {
+        return writeFile('db.json', JSON.stringify(note));
+    }
+
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            }
+            catch (err) {
+                parsedNotes = [];
+            }
+
+            return parsedNotes;
+        });
+    };
+
+    addNote(note) {
+        const {title, text} = note;
+
+        if (!title || !text) {
+            throw new Error("Title and text must both be filled");
+        };
+
+        const newNote = { title, text, id: uniqid() };
+
+        return this.getNotes()
+            .then((notes) => [...notes, newNote])
+            .then((updatedNotes) => this.write(updatedNotes))
+            .then(() => newNote);
+    };
+
+    removeNote() {
+        return this.getNotes()
+            .then((notes) => notes.filter((note) => note.id != id))
+            .then((filteredNotes) => this.write(filteredNotes));
+    }
+}
+
+module.exports = new Store();
--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 // Establishing dependencies
 const express = require('express');
+const router = express.Router()
 const path = require('path');
+const store = require('db/store')
 
 // Assigning express function to object and establishing port number
 const app = express();
 const PORT = 3001;
 
-// Sets an empty array to hold notes data
-const notes = [];
-
 // parsing data in JSON format
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Establishing our routes for different pages
+// Establishing our HTML routes for different pages
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-
+// Serves static files from the public directory
 app.use(express.static('public'));
+
+
+// Establishing API routes
+router.get('/notes', (req, res) => {
+    store
+        .getNotes()
+})
 
 // Sets up server to listen on previously established port 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
